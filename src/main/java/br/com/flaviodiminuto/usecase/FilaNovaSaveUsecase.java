d0d787da96f6774be0fb817f05bcdf0774a405c8
flaviodiminuto <flavio.diminuto@outlook.com>
2022-05-14 00:43:53 -0300

    evitar mais de uma fila por dia

--- src/main/java/br/com/flaviodiminuto/usecase/FilaNovaSaveUsecase.java
+++ src/main/java/br/com/flaviodiminuto/usecase/FilaNovaSaveUsecase.java
@@ -7,6 +7,7 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Optional;
 
 @ApplicationScoped
 public class FilaNovaSaveUsecase {
@@ -15,6 +16,9 @@ public class FilaNovaSaveUsecase {
     FilaRepository repository;
 
     public FilaEntity execute(LocalDate data){
+        Optional<FilaEntity> filaPersistida = repository.findByDataOptional(data);
+        if(filaPersistida.isPresent()) return filaPersistida.get();
+
         FilaEntity fila = FilaEntity.builder()
                 .data(data)
                 .pedidos(new ArrayList<>())
